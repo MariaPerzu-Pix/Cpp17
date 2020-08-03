90e6da6df14591f773075cb28cd83aba55abce5a
MariaPA <maria.perdomoarvizu.de@ieee.org>
2020-08-03 14:20:57 -0700

    type safety violated

--- Code/main.cpp
+++ Code/main.cpp
@@ -8,7 +8,16 @@
 #include <iostream>
 int main()
 {
-	std::cout<<"hello Mari"<< std::endl;
+	union data2{
+		int length;
+		float with;
+	};
+
+	data2 size;
+	size.length =5;
+
+
+	std::cout<<"value is" <<size.with << std::endl;
 }
 
 
