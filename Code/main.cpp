a6495087c37a0c66c20e939c1b1594f0a5cd145b
MariaPA <maria.perdomoarvizu.de@ieee.org>
2020-08-05 16:55:13 -0700

    Example 1 string_view Tour C++

--- Code/main.cpp
+++ Code/main.cpp
@@ -5,28 +5,38 @@
  *      Author: mariapa
  */
 
-#include <any>
+#include <string_view>
+#include <string>
+#include <algorithm>
 #include <iostream>
 
-std::any compose_message(std::string inp)
+using namespace std::literals::string_view_literals;
+
+std::string cat(std::string_view sv1, std::string_view sv2)
 {
-	std::string ok = "all right";
-	int nu = 404;
-	if(inp=="problem")
-		return nu;
-	else
-		return ok;
+	std::string res((sv1.length()+sv2.length()),'*'); //makes a string res(size,inizialization)
+	char* p = &res[0];
+	for(char c : sv1) //for each char in sv1
+		*p++ = c;  // put in res the char of sv1 and go to the next position
+	std::copy(sv2.begin(), sv2.end(),p); // currently p points to rest in he position of the size of sv1, from there put all sv2 (beginning to end)
+	return res;
 }
 
 int main()
 {
-	std::string name = " ";
-	std::cout<< "Enter a name" <<std::endl;
-	std::cin>>name;
+	std::string king = "Harold ";
+	auto s1 = cat(king, "William"); // string and const char*
+	std::cout<< "\n"<<s1;
+	auto s2 = cat(king, king);
+	std::cout<< "\n"<<s2;
+	auto s3 = cat("Charles ", "Philip"sv); //Note the use of the sv (“string view”) suffix. To use that we need to: using namespace std::literals::string_view_literals; lin 13
+	std::cout<< "\n"<<s3;
+	auto s4 = cat("David "sv, king);
+	std::cout<< "\n"<<s4;
+	auto s5 = cat({&king[0],2}, " Henry"sv);
+	std::cout<< "\n"<<s5;
 
-	auto m = compose_message(name);
-    std::cout<< std::any_cast<std::string>(m) <<std::endl;
-    return 0;
+	return 0;
 }
 
 
