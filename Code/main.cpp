c314fc3c1935d26d78c2b53270cdb3961c8658c0
MariaPA <maria.perdomoarvizu.de@ieee.org>
2020-08-14 09:31:16 -0700

    byte shift and to integer

--- Code/main.cpp
+++ Code/main.cpp
@@ -7,27 +7,16 @@
 
 
 #include <iostream>
-#include <filesystem>
+#include <cstddef>
 
-void print_directory(std::filesystem::path p)
-try
-{
-	if(std::filesystem::is_directory(p))
-	{
-		std::cout<<p<< ":\n";
-		for(const std::filesystem::directory_entry& x : std::filesystem::directory_iterator{p})
-			std::cout<< "  "<< x.path() << '\n';
-	}
-}
-catch (const std::filesystem::filesystem_error& ex) {
-	std::cerr<<ex.what()<< '\n';
-}
 
 int main()
 {
-    print_directory("."); // current directory
-    print_directory(".."); //parent directory
-    print_directory("/"); //linux root directory
-    return 0;
+    std::byte b{42};
+    std::cout << std::to_integer<int>(b) << "\n";
+
+    // b *= 2 compilation error
+    b <<= 1;
+    std::cout << std::to_integer<int>(b) << "\n";
 }
 
